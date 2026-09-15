9ef16aa64c2aa2628fb9d6cf5e80cdc0010370c8
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-09-15 11:14:34 +0100

    vai

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -8,14 +8,3 @@ sudo apt install --yes git zsh curl
 
 #
 sh -c "$(curl -#fSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#
-curl -sS https://starship.rs/install.sh | sh
-
-#
-echo '
-# starship
-eval "$(starship init zsh)"' | tee --append ~/.zshrc >/dev/null
-
-#
-starship preset plain-text-symbols -o ~/.config/starship.toml
